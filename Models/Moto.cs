04aa018e8dcd43dd50766be47b7d8ab534f54c6b
Gustavo Araujo <gugagrilo07@gmail.com>
2025-04-23 17:55:57 -0300

    Updated Moto

--- Models/Moto.cs
+++ Models/Moto.cs
@@ -6,6 +6,23 @@
         public string? Chassi { get; set; }
         public string? Placa { get; set; }
         public ModeloMoto Modelo { get; set; }
+
+        public int UwbTagId { get; set; }
+        public UwbTag UwbTag { get; set; }
+
+        public MotoStatus Status { get; set; } = MotoStatus.Disponivel;
+
+        public double? LastX { get; set; }
+        public double? LastY { get; set; }
+        public DateTime? LastSeenAt { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public List<UwbMeasurement> Measurements { get; set; }
+            = new List<UwbMeasurement>();
+        public List<PositionRecord> PositionHistory { get; set; }
+            = new List<PositionRecord>();
     }
 
     public enum ModeloMoto
@@ -14,5 +31,11 @@
         MottuSport,
         MottuE,
         MottuPop
+    }
+    public enum MotoStatus
+    {
+        Disponivel, 
+        Reservada, 
+        EmManutencao,
     }
 }
